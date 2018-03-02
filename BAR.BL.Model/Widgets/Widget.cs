c66987146a979685dcef0f5137c86bafd404c22a
jarne <jarne@LAPTOP-40O2DF4T>
2018-03-02 13:24:20 +0100

    test

---
+++ BAR.BL.Model/Widgets/Widget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAR.BL.Model.Widgets
+{
+    public class Widget
+    {
+        public int WidgetId { get; set; }
+        public string Title { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
