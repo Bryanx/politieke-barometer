4090ad6d1875357230f9088325b0a62cc9f61f46
jarne <jarne@LAPTOP-40O2DF4T>
2018-03-10 13:45:15 +0100

    determineTrending
    interaction diagram "determineTrending" was implemented.

---
+++ BAR.BL/Managers/DataManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BAR.DAL;
+
+namespace BAR.BL.Managers
+{
+    public class DataManager
+    {
+
+        public int GetAanatalInfo(int itemId, DateTime since)
+        {
+            InformationRepository infRepo = new InformationRepository();
+            return infRepo.getAantalInfo(itemId, since);
+        }
+                   
+    }
+}
+
+
