99a8e1b594a61bf29b5c582a14f5662fcc980863
remismeets <remi.1998@hotmail.com>
2018-03-13 20:33:47 +0100

    Changed names

--- BAR.BL/Managers/DataManager.cs
+++ BAR.BL/Managers/DataManager.cs
@@ -13,17 +13,13 @@ namespace BAR.BL.Managers
         /// Gets the number of informations of a specific given item
         /// form since till now.
         /// </summary
-<<<<<<< HEAD
-        public int GetAantalInfo(int itemId, DateTime since)
-=======
         public int GetNumberInfo(int itemId, DateTime since)
->>>>>>> f34955d8e48a371814e65d354b49033fe6d64af3
         {
             IInformationRepository infRepo = new InformationRepository();
             return infRepo.ReadNumberInfo(itemId, since);
         }
 
-        public int GetAanatalInfo(int itemId, DateTime since) {
+        public int GetAantalInfo(int itemId, DateTime since) {
             throw new NotImplementedException();
         }
     }
