24c2fbd0ebccc56c6d0bfa29885504a221c2549e
Maarten Jorens <maarten.jorens@skynet.be>
2018-03-13 13:46:54 +0100

    UserRepository and interface

---
+++ BAR.DAL/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using BAR.BL.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAR.DAL
+{
+  public interface IUserRepository
+  {
+    User ReadUser(int userID);
+  }
+}
---
+++ BAR.DAL/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using BAR.BL.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAR.DAL
+{ 
+  public class UserRepository : IUserRepository
+  {
+    private List<User> users;
+
+    public UserRepository()
+    {
+      users = new List<User>();
+    }
+
+    public User ReadUser(int userID)
+    {
+      User user = users.Where(u => u.UserId == userID).FirstOrDefault();
+
+      return user;
+    }
+  }
+} 
