f55d6d115bf47c3eb15f19dd690cf48154783ba1
Maarten Jorens <maarten.jorens@skynet.be>
2018-03-14 23:35:56 +0100

    Console application menu and features for checking db

--- BAR.BL/Controllers/SysController.cs
+++ BAR.BL/Controllers/SysController.cs
@@ -3,29 +3,34 @@ using System;
 
 namespace BAR.BL.Controllers
 {
-    /// <summary>
-    /// Controller used for backgroundtasks.
-    /// </summary>
-    public class SysController
-    {
-        /// <summary>
-        /// When new data arrives this method will be triggerd to determine
-        /// a new baseline and a new trending percentage for a specific item.
-        /// </summary>
-        public void DetermineTrending(int itemId)
-        {
-            IItemManager itemManager = new ItemManager();
-            itemManager.DetermineTrending(itemId);
-        }
+  /// <summary>
+  /// Controller used for backgroundtasks.
+  /// </summary>
+  public class SysController
+  {
 
-        /// <summary>
-        /// When a new baseline and trendingpercentage is determined,
-        /// Alerts will be generated.
-        /// </summary>
-        public void GenerateAlerts(int itemId)
-        {
-            ISubscriptionManager subManager = new SubscriptionManager();
-            subManager.GenerateAlerts(itemId);
-        }
+    public SysController()
+    {
+
     }
+    /// <summary>
+    /// When new data arrives this method will be triggerd to determine
+    /// a new baseline and a new trending percentage for a specific item.
+    /// </summary>
+    public void DetermineTrending(int itemId)
+    {
+      IItemManager itemManager = new ItemManager();
+      itemManager.DetermineTrending(itemId);
+    }
+
+    /// <summary>
+    /// When a new baseline and trendingpercentage is determined,
+    /// Alerts will be generated.
+    /// </summary>
+    public void GenerateAlerts(int itemId)
+    {
+      ISubscriptionManager subManager = new SubscriptionManager();
+      subManager.GenerateAlerts(itemId);
+    }
+  }
 }
--- UI-CA/Program.cs
+++ UI-CA/Program.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BAR.BL.Managers;
+using BAR.BL.Domain.Items;
+using BAR.BL.Domain.Users;
+using BAR.BL.Controllers;
 
 namespace BAR.UI.CA
 {
@@ -15,20 +18,121 @@ namespace BAR.UI.CA
   /// </summary>
   public class Program
   {
+    private static bool quit = false;
+
+    private static void ShowMenu()
+    {
+      Console.WriteLine("=================================");
+      Console.WriteLine("=== CONSOLE - BAROMETER =========");
+      Console.WriteLine("=================================");
+      Console.WriteLine("1) Toon alle users");
+      Console.WriteLine("2) Toon alerts van een user");
+      Console.WriteLine("3) Voeg nieuwe tweets toe");
+      Console.WriteLine("4) Toon alle items");
+      Console.WriteLine("5) Bepaal baselines en trending voor alle items");
+      Console.WriteLine("0) Afsluiten");
+      try
+      {
+        DetectMenuAction();
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine();
+        Console.WriteLine("Er heeft zich een onverwachte fout voorgedaan!");
+        Console.WriteLine();
+      }
+    }
+
+    private static void DetectMenuAction()
+    {
+      bool inValidAction = false;
+      do
+      {
+        Console.Write("Keuze: ");
+        string input = Console.ReadLine();
+        int action;
+        if (Int32.TryParse(input, out action))
+        {
+          switch (action)
+          {
+            case 1:
+              ShowAllUsers(); break;
+            case 2:
+              ShowAlertsForUser(); break;
+            case 3:
+              AddTweets(); break;
+            case 4:
+              ShowAllItems(); break;
+            case 5:
+              CalculateTrending(); break;
+            case 0:
+              quit = true;
+              return;
+            default:
+              Console.WriteLine("Geen geldige keuze!");
+              inValidAction = true;
+              break;
+          }
+        }
+      } while (inValidAction);
+    }
+
+    private static void CalculateTrending()
+    {
+      
+      SysController sys = new SysController();
+      List<Item> allItems = new ItemManager().getAllItems();
+      for (int i = 0; i < allItems.Count; i++)
+      {
+        sys.DetermineTrending(i+1);
+      }
+    }
+
+    private static void ShowAllItems()
+    {
+      List<Item> allItems = new ItemManager().getAllItems();
+      for (int i=0; i<allItems.Count; i++)
+      {
+        Console.WriteLine(allItems[i].Name + " baseline: " + allItems[i].Baseline + " trendingpercentage: " + allItems[i].TrendingPercentage);
+      }
+    }
+
+    private static void AddTweets()
+    {
+      throw new NotImplementedException();
+    }
+
+    private static void ShowAlertsForUser()
+    {
+      throw new NotImplementedException();
+    }
+
+    private static void ShowAllUsers()
+    {
+      List<User> allUsers = new UserManager().GetAllUsers();
+      for (int i = 0; i < allUsers.Count; i++)
+      {
+        Console.WriteLine(allUsers[i].FirstName + " " + allUsers[i].LastName);
+      }
+    }
+
     public static void Main(string[] args)
     {
+
+      while (!quit)
+        ShowMenu();
       //IUserManager userManager = new UserManager();
       //Console.WriteLine(userManager.GetUser(1).FirstName);
       //Console.ReadKey();
-      BL.Domain.Items.Item item = new ItemManager().GetItem(1);
+      //BL.Domain.Items.Item item = new ItemManager().GetItem(1);
 
-      Console.WriteLine(item.Name);
+      //Console.WriteLine(item.Name);
 
-      var sm = new SubscriptionManager();
-      sm.CreateSubscription(1, 1, 50);
+      //var sm = new SubscriptionManager();
+      //sm.CreateSubscription(1, 1, 50);
 
-      Console.WriteLine("GEDAAN");
-      Console.ReadKey();
+      //Console.WriteLine("GEDAAN");
+      //Console.ReadKey();
 
       //wave 1
       //Are setup is based on the json-dump data.
