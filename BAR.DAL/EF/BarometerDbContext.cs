1713d5223038bd4932e00090b6ce78207a2f261c
jarne <jarne@LAPTOP-40O2DF4T>
2018-03-14 08:45:17 +0100

    Worked on Entity Framwork

---
+++ BAR.DAL/EF/BarometerDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BAR.BL.Domain.Data;
+using BAR.BL.Domain.Items;
+using BAR.BL.Domain.Users;
+using System.Data.Entity;
+
+namespace BAR.DAL.EF
+{
+    public class BarometerDbContext : DbContext
+    {
+        public BarometerDbContext() : base("BAR_DB")
+        {
+            Database.SetInitializer(new BarometerInitializer());
+        }
+
+        //Data package
+        public DbSet<Information> Informations { get; set; }
+
+        //User package
+        public DbSet<User> Users { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
+
+        //Item package
+        public DbSet<Item> Items { get; set; }
+    }
+}
---
+++ BAR.DAL/EF/BarometerInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace BAR.DAL.EF
+{
+    internal class BarometerInitializer : DropCreateDatabaseAlways<BarometerDbContext>
+    {
+        /// <summary>
+        /// Dummy data form the json file will be generated
+        /// in this file for the first wave of information
+        /// </summary>       
+        protected override void Seed(BarometerDbContext context)
+        {
+            //TODO: implement logic
+        }
+    }
+}
