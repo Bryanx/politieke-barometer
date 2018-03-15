95ed764f83e0e158fe421b0e0456bc2f40bcfee9
Maarten Jorens <maarten.jorens@skynet.be>
2018-03-15 13:31:54 +0100

    Added Controller for user alerts

--- UI-MVC/Controllers/UserController.cs
+++ UI-MVC/Controllers/UserController.cs
@@ -1,21 +1,29 @@
-﻿using BAR.BL.Domain.Users;
+﻿using BAR.BL.Controllers;
+using BAR.BL.Domain.Users;
 using BAR.BL.Managers;
 using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace BAR.UI.MVC.Controllers
 {
-    public class UserController : Controller
+  public class UserController : Controller
+  {
+    /// <summary>
+    /// This method is called every time  when a
+    /// user wants to see his/her alers
+    /// </summary>
+    public ActionResult Index(int userId)
     {
-        /// <summary>
-        /// This method is called every time  when a
-        /// user wants to see his/her alers
-        /// </summary>
-        public ActionResult Index(int userId)
-        {
-            ISubscriptionManager subManager = new SubscriptionManager();
-            IEnumerable<Alert> alertsToShow = subManager.GetAllAlerts(userId);
-            return View(alertsToShow);
-        }
+
+      SysController sys = new SysController();
+      
+
+      ISubscriptionManager subManager = new SubscriptionManager();
+      IEnumerable<Alert> alertsToShow = subManager.GetAllAlerts(userId);
+
+      ViewBag.Alerts = alertsToShow;
+
+      return View(ViewBag.Alerts);
     }
+  }
 }
