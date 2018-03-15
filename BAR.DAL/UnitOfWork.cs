0bb1a755e5d5442b37003911dc67eccfdd7228a2
Maarten Jorens <maarten.jorens@skynet.be>
2018-03-15 11:31:11 +0100

    Revert "Revert "Revert "Documentatie Bijgevoegd en spelling gechecked"""
    This reverts commit dd04d1d5ad2671ff86f48eecae9a0e7954864928.

--- BAR.DAL/UnitOfWork.cs
+++ BAR.DAL/UnitOfWork.cs
@@ -9,13 +9,6 @@ namespace BAR.DAL
 	/// Bij de instantatie van de context geven we aan onze context mee dat deze leeft
 	/// binnen een UnitOfWork klasse, zodanig dat de private member 'DelaySave' van de context klasse
 	/// op true komt te staan.
-    /// 
-    /// The UnitOfWork class is used to create a unique context object 
-    /// that is placed over the instances of the repositories.
-    /// there is a private member and an internal property context that
-    /// we can call from anywhere within the same project.
-    /// We give to the instance of the context that our context is inside a UnitOfWork class,
-    /// so the DelaySave parameter of the context class will be 'true'.
 	/// </summary>
 	public class UnitOfWork
 	{
@@ -32,9 +25,7 @@ namespace BAR.DAL
 
 		/// <summary>
 		/// Deze methode zorgt ervoor dat alle tot hier toe aangepaste domein objecten
-		/// worden gepersisteert naar de databank.
-        /// 
-        /// This method will persist all changed domain objects to the database.
+		/// worden gepersisteert naar de databank
 		/// </summary>
 		public void CommitChanges()
 		{
