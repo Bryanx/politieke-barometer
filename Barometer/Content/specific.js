ab942569546f76d5b2b03b04dce95ba8647ca28f
Maarten Jorens <maarten.jorens@skynet.be>
2018-03-16 14:56:41 +0100

    Revert "Clean master"
    This reverts commit 43f97da1d356315b498197429e6d7bcec1e1d457.

---
+++ Barometer/Content/specific.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    $(function () {
+        $('.grid-stack').gridstack();
+    });
+
+
+    ko.components.register('dashboard-grid', {
+        viewModel: {
+            createViewModel: function (controller, componentInfo) {
+                var ViewModel = function (controller, componentInfo) {
+                    var grid = null;
+
+                    this.widgets = controller.widgets;
+
+                    this.afterAddWidget = function (items) {
+                        if (grid == null) {
+                            grid = $(componentInfo.element).find('.grid-stack').gridstack({
+                                auto: false
+                            }).data('gridstack');
+                        }
+
+                        var item = _.find(items, function (i) { return i.nodeType == 1 });
+                        grid.addWidget(item);
+                        ko.utils.domNodeDisposal.addDisposeCallback(item, function () {
+                            grid.removeWidget(item);
+                        });
+                    };
+                };
+
+                return new ViewModel(controller, componentInfo);
+            }
+        },
+        template: { element: 'gridstack-template' }
+    });
+
+    $(function () {
+        var Controller = function (widgets) {
+            var self = this;
+
+            this.widgets = ko.observableArray(widgets);
+
+            this.addNewWidget = function () {
+                this.widgets.push({
+                    x: 0,
+                    y: 0,
+                    width: Math.floor(1 + 3 * Math.random()),
+                    height: Math.floor(1 + 3 * Math.random()),
+                    auto_position: true
+                });
+                return false;
+            };
+
+            this.deleteWidget = function (item) {
+                self.widgets.remove(item);
+                return false;
+            };
+        };
+
+        var widgets = [
+            {x: 0, y: 0, width: 2, height: 2},
+            {x: 2, y: 0, width: 4, height: 2},
+            {x: 6, y: 0, width: 2, height: 4},
+            {x: 1, y: 2, width: 4, height: 2}
+        ];
+
+        var controller = new Controller(widgets);
+        ko.applyBindings(controller);
+    });
+
+
+});
