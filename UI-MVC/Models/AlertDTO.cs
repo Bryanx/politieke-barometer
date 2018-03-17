2db09dca340bb5136de6ad976818e3591b08ec43
Bryan de Ridder <br.deridder@gmail.com>
2018-03-17 12:41:03 +0100

    Adding timestamp to alerts

--- UI-MVC/Models/AlertDTO.cs
+++ UI-MVC/Models/AlertDTO.cs
@@ -8,7 +8,7 @@ namespace BAR.UI.MVC.Models {
         public string Message { get; set; }
         public AlertType AlertType { get; set; }
         public bool IsRead { get; set; }
-        public DateTime? TimeStamp { get; set; }
-        public string Name { get; set; }
+        public long TimeStamp { get; set; }
+        public string Name { get; set; }        
     }
 }
