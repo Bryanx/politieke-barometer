180b08f839728b05df63c14a588a9324f5f7f51d
Bryan de Ridder <br.deridder@gmail.com>
2018-03-18 23:38:51 +0100

    Cleaning up libraries

--- UI-MVC/App_Start/BundleConfig.cs
+++ UI-MVC/App_Start/BundleConfig.cs
@@ -6,13 +6,33 @@ namespace BAR.UI.MVC {
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
-            
-            bundles.Add(new ScriptBundle("~/bundles/timeago").Include(
-                "~/Scripts/jquery.timeago.js"));
+                "~/Scripts/jquery-2.2.4.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/flot").Include(
+                "~/vendors/Flot/jquery.flot.js",
+                "~/vendors/Flot/jquery.flot.pie.js",
+                "~/vendors/Flot/jquery.flot.time.js",
+                "~/vendors/Flot/jquery.flot.stack.js",
+                "~/vendors/Flot/jquery.flot.resize.js",
+                "~/vendors/flot.orderbars/js/jquery.flot.orderBars.js",
+                "~/vendors/flot-spline/js/jquery.flot.spline.min.js",
+                "~/vendors/flot.curvedlines/curvedLines.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/map").Include(
+                "~/vendors/jqvmap/dist/jquery.vmap.min.js",
+                "~/vendors/jqvmap/dist/maps/jquery.vmap.belgium.js",
+                "~/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js")); //TODO: replace smapledata with actual data.
+            
+            bundles.Add(new ScriptBundle("~/bundles/timeago").Include(
+                "~/Scripts/jquery.timeago.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/dates").Include(
+                "~/vendors/DateJS/build/date.js",
+                "~/vendors/moment/min/moment.min.js",
+                "~/vendors/bootstrap-daterangepicker/daterangepicker.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -21,7 +41,30 @@ namespace BAR.UI.MVC {
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
+                "~/Scripts/respond.js",
+                "~/Scripts/bootstrap-progressbar.min.js",
+                "~/Scripts/nprogress.js",
+                "~/Scripts/fastclick.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/gridstack").Include(
+                "~/Scripts/jquery-ui.js",
+                "~/Scripts/underscore.min.js",
+                "~/Scripts/gridstack.js",
+                "~/Scripts/gridstack.jQueryUI.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/morrisCharts").Include(
+                "~/vendors/raphael/raphael.min.js",
+                "~/vendors/morris.js/morris.min.js"
+                ));
+            
+            bundles.Add(new ScriptBundle("~/bundles/pnotify").Include(
+                "~/vendors/pnotify/dist/pnotify.js",
+                "~/vendors/pnotify/dist/pnotify.buttons.js",
+                "~/vendors/pnotify/dist/pnotify.nonblock.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                "~/Content/build/js/custom.js",
+                "~/Content/build/js/searchbar.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css"));
--- UI-MVC/Content/build/js/custom.js
+++ UI-MVC/Content/build/js/custom.js
@@ -210,75 +210,6 @@ $(document).ready(function () {
         });
     }
 });
-// /Switchery
-
-
-// iCheck
-$(document).ready(function () {
-    if ($("input.flat")[0]) {
-        $(document).ready(function () {
-            $('input.flat').iCheck({
-                checkboxClass: 'icheckbox_flat-green',
-                radioClass: 'iradio_flat-green'
-            });
-        });
-    }
-});
-// /iCheck
-
-// Table
-$('table input').on('ifChecked', function () {
-    checkState = '';
-    $(this).parent().parent().parent().addClass('selected');
-    countChecked();
-});
-$('table input').on('ifUnchecked', function () {
-    checkState = '';
-    $(this).parent().parent().parent().removeClass('selected');
-    countChecked();
-});
-
-var checkState = '';
-
-$('.bulk_action input').on('ifChecked', function () {
-    checkState = '';
-    $(this).parent().parent().parent().addClass('selected');
-    countChecked();
-});
-$('.bulk_action input').on('ifUnchecked', function () {
-    checkState = '';
-    $(this).parent().parent().parent().removeClass('selected');
-    countChecked();
-});
-$('.bulk_action input#check-all').on('ifChecked', function () {
-    checkState = 'all';
-    countChecked();
-});
-$('.bulk_action input#check-all').on('ifUnchecked', function () {
-    checkState = 'none';
-    countChecked();
-});
-
-function countChecked() {
-    if (checkState === 'all') {
-        $(".bulk_action input[name='table_records']").iCheck('check');
-    }
-    if (checkState === 'none') {
-        $(".bulk_action input[name='table_records']").iCheck('uncheck');
-    }
-
-    var checkCount = $(".bulk_action input[name='table_records']:checked").length;
-
-    if (checkCount) {
-        $('.column-title').hide();
-        $('.bulk-actions').show();
-        $('.action-cnt').html(checkCount + ' Records Selected');
-    } else {
-        $('.column-title').show();
-        $('.bulk-actions').hide();
-    }
-}
-
 
 // Accordion
 $(document).ready(function () {
@@ -635,7 +566,7 @@ function init_JQVmap() {
     console.log('init_JQVmap');
 
     if ($('#world-map-gdp').length) {
-        let colors = ["#E6F2F0", primary_color];
+        var colors = ["#E6F2F0", primary_color];
         $('#world-map-gdp').vectorMap({
             map: 'be_mill',
             backgroundColor: null,
@@ -2832,94 +2763,7 @@ function init_calendar() {
 
 };
 
-/* DATA TABLES */
 
-function init_DataTables() {
-
-    console.log('run_datatables');
-
-    if (typeof ($.fn.DataTable) === 'undefined') {
-        return;
-    }
-    console.log('init_DataTables');
-
-    var handleDataTableButtons = function () {
-        if ($("#datatable-buttons").length) {
-            $("#datatable-buttons").DataTable({
-                dom: "Bfrtip",
-                buttons: [
-                    {
-                        extend: "copy",
-                        className: "btn-sm"
-                    },
-                    {
-                        extend: "csv",
-                        className: "btn-sm"
-                    },
-                    {
-                        extend: "excel",
-                        className: "btn-sm"
-                    },
-                    {
-                        extend: "pdfHtml5",
-                        className: "btn-sm"
-                    },
-                    {
-                        extend: "print",
-                        className: "btn-sm"
-                    },
-                ],
-                responsive: true
-            });
-        }
-    };
-
-    TableManageButtons = function () {
-        "use strict";
-        return {
-            init: function () {
-                handleDataTableButtons();
-            }
-        };
-    }();
-
-    $('#datatable').dataTable();
-
-    $('#datatable-keytable').DataTable({
-        keys: true
-    });
-
-    $('#datatable-responsive').DataTable();
-
-    $('#datatable-scroller').DataTable({
-        ajax: "js/datatables/json/scroller-demo.json",
-        deferRender: true,
-        scrollY: 380,
-        scrollCollapse: true,
-        scroller: true
-    });
-
-    $('#datatable-fixed-header').DataTable({
-        fixedHeader: true
-    });
-
-    var $datatable = $('#datatable-checkbox');
-
-    $datatable.dataTable({
-        'order': [[1, 'asc']],
-        'columnDefs': [
-            {orderable: false, targets: [0]}
-        ]
-    });
-    $datatable.on('draw.dt', function () {
-        $('checkbox input').iCheck({
-            checkboxClass: 'icheckbox_flat-green'
-        });
-    });
-
-    TableManageButtons.init();
-
-};
 
 /* CHART - MORRIS  */
 
@@ -5393,7 +5237,6 @@ function callMethods() {
     init_skycons();
     init_select2();
     init_validator();
-    init_DataTables();
     init_chart_doughnut();
     init_gauge();
     init_PNotify();
