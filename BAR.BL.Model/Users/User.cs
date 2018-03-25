e3cb0da4502726d4d5f40270b187f3e0784b4b33
Bryan <br.deridder@gmail.com>
2018-03-25 21:56:32 +0200

    Added alert and account settings

--- BAR.BL.Model/Users/User.cs
+++ BAR.BL.Model/Users/User.cs
@@ -12,10 +12,14 @@ namespace BAR.BL.Domain.Users
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string EmailAdress { get; set; }
+		public bool AlertsViaEmail { get; set; }
+		public bool AlertsViaWebsite { get; set; }
+		public bool WeeklyReviewViaEmail { get; set; }
 		public Gender Gender { get; set; }
 		public DateTime? DateOfBirth { get; set; }
 		public Role Role { get; set; }
 		public Area Area { get; set; }
 		public ICollection<Activity> Activities { get; set; }
+		public bool Deleted { get; set; }
 	}
 }
