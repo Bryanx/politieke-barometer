cf8cf62683bfc01fcb4eff83748142b551b7b433
jarne <jarne@LAPTOP-40O2DF4T>
2018-03-27 18:24:31 +0200

    setup

---
+++ BAR.BL/Managers/DashboardManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BAR.BL.Domain.Widgets;
+using BAR.DAL;
+
+namespace BAR.BL.Managers
+{
+	/// <summary>
+	/// Responsable for managing widgets
+	/// and their dashboards 
+	/// </summary>
+	public class DashboardManager : IDashboardManager
+	{
+		private IDashboardRepository infoRepo;
+		private UnitOfWorkManager uowManager;
+
+		/// <summary>
+		/// When unit of work is present, it will effect
+		/// initRepo-method. (see documentation of initRepo)
+		/// </summary>
+		public DashboardManager(UnitOfWorkManager uowManager = null)
+		{
+			this.uowManager = uowManager;
+		}
+
+		/// <summary>
+		/// Gives back a list of
+		/// </summary>
+		public IEnumerable<Widget> GetWidgets(int dashboardId)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Determines if the repo needs a unit of work
+		/// if the unitOfWorkManager is present.
+		/// </summary>
+		private void InitRepo()
+		{
+			if (uowManager == null) infoRepo = new DashboardRepository();
+			else infoRepo = new DashboardRepository(uowManager.UnitOfWork);
+		}
+	}
+}
---
+++ BAR.BL/Managers/IDashboardManager.cs
@@ -0,0 +1,14 @@
+﻿using BAR.BL.Domain.Widgets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAR.BL.Managers
+{
+	public interface IDashboardManager
+	{
+		IEnumerable<Widget> GetWidgets(int dashboardId);
+	}
+}
