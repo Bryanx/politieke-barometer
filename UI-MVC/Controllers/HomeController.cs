4b3cd0438e9d9a9f42c691d348b6adeeb896c5a9
Bryan <br.deridder@gmail.com>
2018-03-31 17:59:35 +0200

    homepage change ViewModel

--- UI-MVC/Controllers/HomeController.cs
+++ UI-MVC/Controllers/HomeController.cs
@@ -7,12 +7,14 @@ using System.Web.Mvc.Ajax;
 using BAR.BL.Domain.Items;
 using BAR.BL.Managers;
 using BAR.UI.MVC.Models;
+using Microsoft.AspNet.Identity;
 
 namespace BAR.UI.MVC.Controllers {
     public class HomeController : Controller {
-        
+        private const string INDEX_PAGE_TITLE = "Politieke Barometer";
         IItemManager itemMgr = new ItemManager();
         
+        [AllowAnonymous]
         public ActionResult Index() {
             List<ItemDTO> personen = new List<ItemDTO>();
             foreach (Item item in itemMgr.GetAllItems()) {
@@ -28,7 +30,14 @@ namespace BAR.UI.MVC.Controllers {
                     Baseline = item.Baseline
                 });
             }
-            return View("Index", "~/Views/Shared/Layouts/_HomeLayout(temp).cshtml", personen.AsEnumerable());
+            UserManager userManager = new UserManager();
+            ItemViewModel itemViewModel = new ItemViewModel() {
+                PageTitle = INDEX_PAGE_TITLE,
+                User = User.Identity.IsAuthenticated ? userManager.GetUser(User.Identity.GetUserId()) : null,
+                People = personen
+            };
+            return View("Index","~/Views/Shared/Layouts/_HomeLayout(temp).cshtml", itemViewModel);
         }
+
     }
 }
