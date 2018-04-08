4c8ed6ff213d869ef5a81d3be2ef85192d5d3690
Bryan de Ridder <br.deridder@gmail.com>
2018-04-08 13:30:09 +0200

    Admin settings ui merge into master (#33)
    * Added admin menu
    
    * Added SetRolesToVm method for checking user roles.
    
    * Added admin pages
    
    * Added user table in user management
    
    * Replaced all urls with helpers
    
    * Remove temp user grid

---
+++ UI-MVC/Helpers/UrlExtensions.cs
@@ -0,0 +1,97 @@
+﻿using System.Web.Mvc;
+using BAR.UI.MVC.Controllers;
+
+namespace BAR.UI.MVC.Helpers {
+    
+    /// <summary>
+    /// These helpers act as a table of contents of all URLs on the website.
+    /// If a Controller/action is refactored it should be changed here.
+    /// </summary>
+    public static class UrlExtensions {
+        public static string RootUrl(this UrlHelper helper) {
+            return helper.RouteUrl("Default", new {
+                controller = "Home",
+                action = nameof(HomeController.Index)
+            });
+        }
+
+        public static string NewsUrl(this UrlHelper helper) {
+            return RootUrl(helper) + "#nieuws";
+        }
+
+        #region UserUrls
+        
+        public static string DashboardUrl(this UrlHelper helper) {
+            return helper.RouteUrl("Default", new {
+                controller = "User",
+                action = nameof(UserController.Index)
+            });
+        }
+
+        public static string SettingsUrl(this UrlHelper helper) {
+            return helper.RouteUrl("Default", new {
+                controller = "User",
+                action = nameof(UserController.Settings)
+            });
+        }
+        
+        #endregion
+
+        #region ItemUrls
+        
+        public static string PersonUrl(this UrlHelper helper) {
+            return helper.RouteUrl("Default", new {
+                controller = "Person",
+                action = nameof(PersonController.Index)
+            });
+        }
+
+        public static string PersonUrl(this UrlHelper helper, int personId) {
+            return helper.RouteUrl("Default", new {
+                controller = "Person",
+                action = nameof(PersonController.Details),
+                id = personId
+            });
+        }
+
+        public static string OrganisationUrl(this UrlHelper helper) {
+            return helper.RouteUrl("Default", new {
+                controller = "Organisation",
+                action = nameof(OrganisationController.Index)
+            });
+        }
+        #endregion
+        
+        #region AdminUrls
+
+        public static string AdminDashboard(this UrlHelper helper) {
+            return helper.RouteUrl("Default", new {
+                controller = "Admin",
+                action = nameof(AdminController.Index)
+            });
+        }
+
+        public static string AdminPageManagement(this UrlHelper helper) {
+            return helper.RouteUrl("Default", new {
+                controller = "Admin",
+                action = nameof(AdminController.PageManagement)
+            });
+        }
+
+        public static string AdminItemManagement(this UrlHelper helper) {
+            return helper.RouteUrl("Default", new {
+                controller = "Admin",
+                action = nameof(AdminController.ItemManagement)
+            });
+        }
+
+        public static string AdminUserManagement(this UrlHelper helper) {
+            return helper.RouteUrl("Default", new {
+                controller = "Admin",
+                action = nameof(AdminController.UserManagement)
+            });
+        }
+        
+        #endregion
+    }
+}
---
+++ UI-MVC/Models/AdminViewModels.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using BAR.BL.Domain.Users;
+
+namespace BAR.UI.MVC.Models {
+    public class EditUserViewModel : BaseViewModel {
+        public IEnumerable<User> Users { get; set; }
+
+        public EditUserViewModel(BaseViewModel vm, IEnumerable<User> users) {
+            PageTitle = vm.PageTitle;
+            ContactStreet = vm.ContactStreet;
+            ContactCity = vm.ContactCity;
+            ContactEmail = vm.ContactEmail;
+            User = vm.User;
+            IsAdmin = vm.IsAdmin;
+            IsSuperAdmin = vm.IsSuperAdmin;
+            Users = users;
+        }
+    }
+}
