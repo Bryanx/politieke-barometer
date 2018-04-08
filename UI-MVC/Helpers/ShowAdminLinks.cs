3f06f2b78501e21325dcebdc8814ff2e513da13f
Bryan <br.deridder@gmail.com>
2018-04-09 01:11:36 +0200

    added helpers for navbarlinks

---
+++ UI-MVC/Helpers/ShowAdminLinks.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Principal;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using BAR.UI.MVC.Models;
+
+namespace BAR.UI.MVC.Helpers {
+    public static class NavBarHelpers {
+        public static MvcHtmlString ShowAdminLinks(this HtmlHelper helper, IPrincipal user) {
+            if (user.IsInRole("Admin") || user.IsInRole("SuperAdmin")) {
+                return helper.Partial("Partials/NavBarAdminLinks");
+            }
+            return MvcHtmlString.Empty;
+        }
+
+        public static MvcHtmlString ShowSuperAdminLinks(this HtmlHelper helper, IPrincipal user) {
+            if (user.IsInRole("SuperAdmin")) {
+                return helper.Partial("Partials/NavBarSuperAdminLinks");
+            }
+            return MvcHtmlString.Empty;
+        }
+    }
+}
