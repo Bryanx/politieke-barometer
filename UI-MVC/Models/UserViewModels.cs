9297966c00eae383da95826327d4004fb5be80d8
Bryan <br.deridder@gmail.com>
2018-04-09 12:44:31 +0200

    Added firstname to register

--- UI-MVC/Models/UserViewModels.cs
+++ UI-MVC/Models/UserViewModels.cs
@@ -70,7 +70,8 @@ namespace BAR.UI.MVC.Models
     [EmailAddress]
     [Display(Name = "Email*")]
     public string Email { get; set; }
-
+    
+    [Required]
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
     [Display(Name = "Voornaam")]
     public string Firstname { get; set; }
