f741b7fb44f6f31527be14b286b407754bdcd7e6
Bryan <br.deridder@gmail.com>
2018-04-09 10:04:56 +0200

    Added helper for footer register

--- UI-MVC/Helpers/NavBarHelpers.cs
+++ UI-MVC/Helpers/NavBarHelpers.cs
@@ -6,22 +6,23 @@ using BAR.UI.MVC.Models;
 namespace BAR.UI.MVC.Helpers {
     public static class NavBarHelpers {
         public static MvcHtmlString ShowAdminLinks(this HtmlHelper helper, IPrincipal user) {
-            if (user.IsInRole("Admin") || user.IsInRole("SuperAdmin")) {
-                return helper.Partial("Partials/NavBarAdminLinks");
-            }
+            if (user.IsInRole("Admin") || user.IsInRole("SuperAdmin")) return helper.Partial("Partials/NavBarAdminLinks");
             return MvcHtmlString.Empty;
         }
 
         public static MvcHtmlString ShowSuperAdminLinks(this HtmlHelper helper, IPrincipal user) {
-            if (user.IsInRole("SuperAdmin")) {
-                return helper.Partial("Partials/NavBarSuperAdminLinks");
-            }
+            if (user.IsInRole("SuperAdmin")) return helper.Partial("Partials/NavBarSuperAdminLinks");
             return MvcHtmlString.Empty;
         }
         
         public static MvcHtmlString ShowNavLogin(this HtmlHelper helper, BaseViewModel model) {
             if (model.User != null) return helper.Partial("Partials/LoggedInNavbar", model);
-            return helper.Partial("Partials/LoginModal", model);;
+            return helper.Partial("Partials/LoginModal", model);
+        }
+
+        public static MvcHtmlString FooterHelper(this HtmlHelper helper, BaseViewModel model) {
+            if (model.User == null) return helper.Partial("Partials/FooterRegister", model);
+            return MvcHtmlString.Empty;
         }
     }
 }
