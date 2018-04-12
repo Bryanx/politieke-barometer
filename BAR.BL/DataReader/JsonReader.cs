2536a9742b534252c33a6ccf4d96d958fd27d91c
Maarten Jorens <maarten.jorens@skynet.be>
2018-04-12 14:54:23 +0200

    JsonReader class for ReadJson in BL

---
+++ BAR.BL/DataReader/JsonReader.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAR.BL.DataReader
+{
+  public class JsonReader
+  {
+    public void ReadJson()
+    {
+      string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data.json");
+      string json = File.ReadAllText(path);
+      dynamic deserializedJson = JsonConvert.DeserializeObject(json);
+
+      Console.WriteLine("JSONREADER FINISHED");
+    }
+  }
+}
