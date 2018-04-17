94012fdc374fd2382b4c276a3d46084312723a7d
Maarten Jorens <maarten.jorens@skynet.be>
2018-04-17 09:59:30 +0200

    SubPLatformCheckAttribute

---
+++ UI-MVC/Attributes/SubPLatformCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BAR.UI.MVC.Attributes
+{
+  public class SubPlatformCheck : System.Web.Mvc.ActionFilterAttribute
+  {
+    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+      string subdomain = GetSubDomain(HttpContext.Current.Request.Url);
+    }
+
+    private static string GetSubDomain(Uri url)
+    {
+      string host = url.Host;
+      if (host.Split('.').Length > 1)
+      {
+        int index = host.IndexOf(".");
+        string subdomain = host.Substring(0, index);
+        if (subdomain != "www")
+        {
+          return subdomain;
+        }
+      }
+      return null;
+    }
+  }
+}
