5efa06cf479ae66f7fd94ff603131a0fa4d352d4
jarne <jarne@LAPTOP-40O2DF4T>
2018-04-17 11:29:58 +0200

    Changed enum

--- BAR.BL.Model/Widgets/ItemWidget.cs
+++ BAR.BL.Model/Widgets/ItemWidget.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace BAR.BL.Domain.Widgets
 {
+	/// <summary>
+	/// Needs to be created to determine te diffirence between a UserWidget and an ItemWidget
+	/// </summary>
 	public class ItemWidget : Widget
 	{
 	}
