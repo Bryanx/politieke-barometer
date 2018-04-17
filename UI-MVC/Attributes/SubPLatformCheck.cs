36ca19151f5ace6a3c1ce1b09c6d57e579324eeb
Maarten Jorens <maarten.jorens@skynet.be>
2018-04-17 10:01:40 +0200

    Using update SubPlatformCheck attribute

--- UI-MVC/Attributes/SubPLatformCheck.cs
+++ UI-MVC/Attributes/SubPLatformCheck.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace BAR.UI.MVC.Attributes
 {
-  public class SubPlatformCheck : System.Web.Mvc.ActionFilterAttribute
+  public class SubPlatformCheck : ActionFilterAttribute
   {
     public override void OnActionExecuting(ActionExecutingContext filterContext)
     {
