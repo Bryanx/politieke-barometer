46b23403c24e54d7c26f795c6b54f32340a17207
Maarten Jorens <maarten.jorens@skynet.be>
2018-04-20 14:48:44 +0200

    Fixed indentation

--- UI-MVC/Controllers/HomeController.cs
+++ UI-MVC/Controllers/HomeController.cs
@@ -16,95 +16,95 @@ using BAR.BL.Domain.Core;
 
 namespace BAR.UI.MVC.Controllers
 {
-	/// <summary>
-	/// This controller is used for managing the homepage.
-	/// </summary>
-	public class HomeController : LanguageController
-	{
-		private const string INDEX_PAGE_TITLE = "Politieke Barometer";
-		private IItemManager itemManager;
-		private IUserManager userManager;
-		private ISubplatformManager subplatformManager;
+  /// <summary>
+  /// This controller is used for managing the homepage.
+  /// </summary>
+  public class HomeController : LanguageController
+  {
+    private const string INDEX_PAGE_TITLE = "Politieke Barometer";
+    private IItemManager itemManager;
+    private IUserManager userManager;
+    private ISubplatformManager subplatformManager;
 
-		/// <summary>
-		/// Landing page for logged-in and non-logged-in users.
-		/// </summary>
-		[AllowAnonymous]
-		public ActionResult Index()
-		{
-			userManager = new UserManager();
-			itemManager = new ItemManager();
+    /// <summary>
+    /// Landing page for logged-in and non-logged-in users.
+    /// </summary>
+    [AllowAnonymous]
+    public ActionResult Index()
+    {
+      userManager = new UserManager();
+      itemManager = new ItemManager();
 
-			//Assembling the view
-			return View(new ItemViewModel()
-			{
-				PageTitle = INDEX_PAGE_TITLE,
-				User = User.Identity.IsAuthenticated ? userManager.GetUser(User.Identity.GetUserId()) : null,
-				Items = Mapper.Map<IList<Item>, IList<ItemDTO>>(itemManager.GetAllItems().ToList())
-			});
-		}
-		/// <summary>
-		/// Privacy page for logged-in and non-logged-in users.
-		/// </summary>
-		[AllowAnonymous]
-		public ActionResult Privacy()
-		{
-			userManager = new UserManager();
+      //Assembling the view
+      return View(new ItemViewModel()
+      {
+        PageTitle = INDEX_PAGE_TITLE,
+        User = User.Identity.IsAuthenticated ? userManager.GetUser(User.Identity.GetUserId()) : null,
+        Items = Mapper.Map<IList<Item>, IList<ItemDTO>>(itemManager.GetAllItems().ToList())
+      });
+    }
+    /// <summary>
+    /// Privacy page for logged-in and non-logged-in users.
+    /// </summary>
+    [AllowAnonymous]
+    public ActionResult Privacy()
+    {
+      userManager = new UserManager();
 
-			//Assembling the view
-			return View(new BaseViewModel()
-			{
-				PageTitle = Resources.PrivacyAndSafety,
-				User = User.Identity.IsAuthenticated ? userManager.GetUser(User.Identity.GetUserId()) : null
-			});
-		}
+      //Assembling the view
+      return View(new BaseViewModel()
+      {
+        PageTitle = Resources.PrivacyAndSafety,
+        User = User.Identity.IsAuthenticated ? userManager.GetUser(User.Identity.GetUserId()) : null
+      });
+    }
 
-		/// <summary>
-		/// FAQ page for logged-in and non-logged-in users.
-		/// </summary>
-		[AllowAnonymous]
-		public ActionResult Faq()
-		{
-			userManager = new UserManager();
+    /// <summary>
+    /// FAQ page for logged-in and non-logged-in users.
+    /// </summary>
+    [AllowAnonymous]
+    public ActionResult Faq()
+    {
+      userManager = new UserManager();
 
-			//Assembling the view
-			return View(new BaseViewModel()
-			{
-				PageTitle = Resources.QuestionAndAnswer,
-				User = User.Identity.IsAuthenticated ? userManager.GetUser(User.Identity.GetUserId()) : null
-			});
-		}
+      //Assembling the view
+      return View(new BaseViewModel()
+      {
+        PageTitle = Resources.QuestionAndAnswer,
+        User = User.Identity.IsAuthenticated ? userManager.GetUser(User.Identity.GetUserId()) : null
+      });
+    }
 
-		/// <summary>
-		/// Saves the language preferences in a cookie.
-		/// </summary>
-		public ActionResult SetCulture(string culture)
-		{
-			// Validate input
-			culture = LanguageHelper.GetImplementedCulture(culture);
-			// Save culture in a cookie
-			HttpCookie cookie = Request.Cookies["_culture"];
-			if (cookie != null)
-				cookie.Value = culture;   // update cookie value
-			else
-			{
-				cookie = new HttpCookie("_culture");                
-				cookie.Value = culture;
-				cookie.Expires = DateTime.Now.AddYears(1);
-			}
-			Response.Cookies.Add(cookie);
-			return Redirect(Request.UrlReferrer.ToString());
-		}      
-		
-		/// <summary>
-		/// Returns a javascript object with language resources.
-		/// This way local resources can be used in javascript files.
-		/// </summary>
-				public ActionResult GetResources()
-				{
-						Response.ContentType = "text/javascript";
-						return View();
-				}
+    /// <summary>
+    /// Saves the language preferences in a cookie.
+    /// </summary>
+    public ActionResult SetCulture(string culture)
+    {
+      // Validate input
+      culture = LanguageHelper.GetImplementedCulture(culture);
+      // Save culture in a cookie
+      HttpCookie cookie = Request.Cookies["_culture"];
+      if (cookie != null)
+        cookie.Value = culture;   // update cookie value
+      else
+      {
+        cookie = new HttpCookie("_culture");
+        cookie.Value = culture;
+        cookie.Expires = DateTime.Now.AddYears(1);
+      }
+      Response.Cookies.Add(cookie);
+      return Redirect(Request.UrlReferrer.ToString());
+    }
 
-		}
+    /// <summary>
+    /// Returns a javascript object with language resources.
+    /// This way local resources can be used in javascript files.
+    /// </summary>
+    public ActionResult GetResources()
+    {
+      Response.ContentType = "text/javascript";
+      return View();
+    }
+
+  }
 }
