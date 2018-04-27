08717b82e4899be38f096842faface962b1426d1
YoniGovaerts <Yoni.Govaerts@student.kdg.be>
2018-04-27 11:27:14 +0200

    Namespace fix

--- UI-MVC/App_GlobalResources/Resources.Designer.cs
+++ UI-MVC/App_GlobalResources/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Resources {
+namespace BAR.UI.MVC.App_GlobalResources {
     using System;
     
     
