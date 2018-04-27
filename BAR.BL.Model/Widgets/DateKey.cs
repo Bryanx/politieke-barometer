347759df44a12c6f074bf397847930a2e7221e65
jarne <jarne@LAPTOP-40O2DF4T>
2018-04-27 15:04:09 +0200

    Addded classes for mapping dictionary to nav-prop

---
+++ BAR.BL.Model/Widgets/DateKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAR.BL.Domain.Widgets
+{
+	public class DateKey
+	{
+		public int DateKeyId { get; set; }
+		public string Date { get; set; }
+	}
+}
---
+++ BAR.BL.Model/Widgets/ValueNumberOfTimes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAR.BL.Domain.Widgets
+{
+	public class ValueNumberOfTimes
+	{
+		public int ValueNumberOfTimesId { get; set; }
+		public double NumberOfTimes { get; set; }
+	}
+}
