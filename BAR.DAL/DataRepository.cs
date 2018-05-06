3221460f343e367904c02635ec32da23e4422b2a
jarne <jarne@LAPTOP-40O2DF4T>
2018-05-06 13:32:38 +0200

    Cleaned up datarepository and interface

--- BAR.DAL/DataRepository.cs
+++ BAR.DAL/DataRepository.cs
@@ -36,7 +36,7 @@ namespace BAR.DAL
 		/// persists it to the database.
 		/// Returns -1 if SaveChanges() is delayed by unit of work.
 		/// </summary>
-		public int CreateInformations(List<Information> infos)
+		public int CreateInformations(IEnumerable<Information> infos)
 		{
 			ctx.Configuration.AutoDetectChangesEnabled = false;
 			ctx.Informations.AddRange(infos);
@@ -72,11 +72,12 @@ namespace BAR.DAL
 		/// </summary>
 		public int DeleteInformations(IEnumerable<int> infoIds)
 		{
-			foreach (int infoId in infoIds)
-			{
-				Information infoToDelete = ReadInformationWitlAllInfo(infoId);
-				ctx.Informations.Remove(infoToDelete);
-			}
+			//Get informations to delete
+			List<Information> infosToDelete = new List<Information>();
+			foreach (int infoId in infoIds) infosToDelete.Add(ReadInformationWitlAllInfo(infoId));
+			
+			//Delete informations
+			ctx.Informations.RemoveRange(infosToDelete);
 			return ctx.SaveChanges();
 		}
 
@@ -87,8 +88,8 @@ namespace BAR.DAL
 		public IEnumerable<Information> ReadInformationsForItemid(int itemId)
 		{
 			return ctx.Informations.Include(info => info.Items)
-										.Where(info => info.Items.Any(item => item.ItemId == itemId))
-									   .AsEnumerable();
+								   .Where(info => info.Items.Any(item => item.ItemId == itemId))
+								   .AsEnumerable();
 		}
 
 		/// <summary>
@@ -117,17 +118,6 @@ namespace BAR.DAL
 			return ctx.Informations.Include(info => info.PropertieValues)
 								   .Include(info => info.PropertieValues.Select(propval => propval.Property))
 								   .Where(info => info.InformationId == informationId).SingleOrDefault();
-		}
-
-		/// <summary>
-		/// Gives back all informations objects form now until a given date
-		/// for a specific item.
-		/// </summary>
-		public IEnumerable<Information> ReadInformationsForDate(int itemId, DateTime since)
-		{
-			return ctx.Informations
-				.Where(info => info.Items.Any(item => item.ItemId == itemId))
-				.Where(info => info.CreationDate >= since).AsEnumerable();
 		}
 
 		/// <summary>
@@ -161,64 +151,100 @@ namespace BAR.DAL
 			return ctx.SaveChanges();
 		}
 
+		/// <summary>
+		/// Gives back a property by propertyname
+		/// </summary>
 		public Property ReadProperty(string propertyName)
 		{
-			return ctx.Properties.Where(x => x.Name.Equals(propertyName)).SingleOrDefault();
+			return ctx.Properties.Where(prop => prop.Name.ToLower().Equals(propertyName.ToLower()))
+								 .SingleOrDefault();
 		}
-
+		
+		/// <summary>
+		/// Gives back a scource by sourcename
+		/// </summary>
 		public Source ReadSource(string sourceName)
 		{
-			return ctx.Sources.Where(x => x.Name.Equals(sourceName)).SingleOrDefault();
+			return ctx.Sources.Where(src => src.Name.ToLower().Equals(sourceName.ToLower()))
+							  .SingleOrDefault();
 		}
 
+		/// <summary>
+		/// Creates a source and persists that to the database
+		/// </summary
 		public int CreateSource(Source source)
 		{
 			ctx.Sources.Add(source);
 			return ctx.SaveChanges();
 		}
 
+		/// <summary>
+		/// Gives back a source
+		/// </summary>
 		public Source ReadSource(int sourceId)
 		{
-			return ctx.Sources.Where(x => x.SourceId == sourceId).SingleOrDefault();
+			return ctx.Sources.Find(sourceId);
 		}
 
+		/// <summary>
+		/// Removes a source from the database
+		/// </summary>
 		public int DeleteSource(Source source)
 		{
 			ctx.Sources.Remove(source);
 			return ctx.SaveChanges();
 		}
 
-
+		/// <summary>
+		/// Returns the first audit from the database
+		/// </summary>
 		public SynchronizeAudit ReadLastAudit()
 		{
-			return ctx.SynchronizeAudits.Where(x => x.Succes).OrderByDescending(x => x.TimeStamp).FirstOrDefault();
+			return ctx.SynchronizeAudits.Where(audit => audit.Succes)
+										.OrderByDescending(audit => audit.TimeStamp)
+										.FirstOrDefault();
 		}
 
-		public int CreateAudit(SynchronizeAudit synchronizeAudit)
+		/// <summary>
+		/// Creates an audit in the database
+		/// </summary>
+		public int CreateAudit(SynchronizeAudit audit)
 		{
-			ctx.SynchronizeAudits.Add(synchronizeAudit);
+			ctx.SynchronizeAudits.Add(audit);
 			return ctx.SaveChanges();
 		}
 
-		public SynchronizeAudit ReadAudit(int synchronizeAuditId)
+		/// <summary>
+		/// Gives back an audit
+		/// </summary>
+		public SynchronizeAudit ReadAudit(int auditId)
 		{
-			return ctx.SynchronizeAudits.Where(x => x.SynchronizeAuditId == synchronizeAuditId).SingleOrDefault();
+			return ctx.SynchronizeAudits.Find(auditId);
 		}
 
-		public int UpdateAudit(SynchronizeAudit synchronizeAudit)
+		/// <summary>
+		/// Updates an audit and persists that to the database
+		/// </summary>
+		public int UpdateAudit(SynchronizeAudit audit)
 		{
-			ctx.Entry(synchronizeAudit).State = EntityState.Modified;
+			ctx.Entry(audit).State = EntityState.Modified;
 			return ctx.SaveChanges();
 		}
 
+		/// <summary>
+		/// Gives back all the properties
+		/// </summary>
 		public IEnumerable<Property> ReadAllProperties()
 		{
-			return ctx.Properties.ToList();
+			return ctx.Properties.AsEnumerable();
 		}
 
+		/// <summary>
+		/// Gives back all the sources
+		/// </summary>
 		public IEnumerable<Source> ReadAllSources()
 		{
-			return ctx.Sources.ToList();
+			return ctx.Sources.AsEnumerable();
 		}
 
 		/// <summary>
