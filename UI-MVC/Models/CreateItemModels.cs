1674dfc51e1aafa816a765e112134f5322c3c4fd
Maarten Jorens <maarten.jorens@skynet.be>
2018-05-09 16:44:50 +0200

    HTTPS change and Model for CreateItem

---
+++ UI-MVC/Models/CreateItemModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using BAR.UI.MVC.App_GlobalResources;
+
+namespace BAR.UI.MVC.Models
+{
+	public class CreateItemModels
+	{
+		public class CreatePersonModel : BaseViewModel
+		{
+			[Required]
+			[Display(Name = "Name", ResourceType = typeof(Resources))]
+			public string Name { get; set; }
+		}
+	}
+}
