8501f45e3a5ec132605fb4ada39a5d3f9fe3c91a
YoniGovaerts <Yoni.Govaerts@student.kdg.be>
2018-05-15 15:45:16 +0200

    Added DataSource

---
+++ BAR.BL.Model/Data/DataSource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAR.BL.Domain.Data
+{
+    class DataSource
+    {
+    }
+}
