6b53ca16f218a57638441e12c375bb2e03cd9484
Rémi <remi.1998@hotmail.com>
2018-05-15 16:13:00 +0200

    Added custom error page

--- UI-MVC/Controllers/ErrorController.cs
+++ UI-MVC/Controllers/ErrorController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BAR.BL.Managers;
+using BAR.UI.MVC.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +11,25 @@ namespace BAR.UI.MVC.Controllers
 {
   public class ErrorController : Controller
   {
+    private IUserManager userManager;
     public ViewResult Index()
     {
-      return View("Error");
+      userManager = new UserManager();
+      return View(new BaseViewModel()
+      {
+        PageTitle = "Test index",
+        User = User.Identity.IsAuthenticated ? userManager.GetUser(User.Identity.GetUserId()) : null
+      });
     }
     public ViewResult NotFound()
     {
+      userManager = new UserManager();
       Response.StatusCode = 404;
-      return View("NotFound");
+      return View(new BaseViewModel()
+      {
+        PageTitle = "Test",
+        User = User.Identity.IsAuthenticated ? userManager.GetUser(User.Identity.GetUserId()) : null
+      });
     }
   }
 }
