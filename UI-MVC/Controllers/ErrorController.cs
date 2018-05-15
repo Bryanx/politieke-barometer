69c17877085226a011868776eb212edbab62703c
Rémi <remi.1998@hotmail.com>
2018-05-15 15:17:28 +0200

    Added fancy pancy error page

---
+++ UI-MVC/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BAR.UI.MVC.Controllers
+{
+  public class ErrorController : Controller
+  {
+    public ViewResult Index()
+    {
+      return View("Error");
+    }
+    public ViewResult NotFound()
+    {
+      Response.StatusCode = 404;
+      return View("NotFound");
+    }
+  }
+}
