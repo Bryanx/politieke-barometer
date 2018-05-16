79ba3558ff89bd891d2f60f2fb053e95b2fc98d0
Anthony <torfs.anthony@hotmail.com>
2018-05-16 13:36:22 +0200

    and again

--- BAR.BL/Managers/IWidgetManager.cs
+++ BAR.BL/Managers/IWidgetManager.cs
@@ -20,7 +20,7 @@ namespace BAR.BL.Managers
 		IEnumerable<Widget> GetAllWidgetsWithAllData();
 		IEnumerable<Widget> GetWidgetsForItem(int itemId);
 		IEnumerable<Widget> GetAllWidgetsWithAllDataForItem(int itemId);
-		IEnumerable<Widget> GetWidgetsForWeeklyReview(int platformId, string userId = null);
+		IEnumerable<Widget> GetWidgetsForWeeklyReview(string userId = null, int platformId = 2);
 
 		Widget AddWidget(WidgetType widgetType, string title, int rowNbr, int colNbr, List<PropertyTag> proptags,
 			DateTime? timestamp = null, GraphType? graphType = null, int rowspan = 1, int colspan = 1, int dashboardId = -1, List<WidgetData> datas = null, List<Item> items = null);
