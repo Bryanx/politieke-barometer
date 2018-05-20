d1c057334f8ec269b105ac0615df3ace55618b57
jarne <jarne@LAPTOP-40O2DF4T>
2018-05-20 13:07:45 +0200

    Refactored global.asax and startup.cs

--- UI-MVC/Global.asax.cs
+++ UI-MVC/Global.asax.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -11,69 +9,82 @@ using BAR.BL.Domain.Items;
 using BAR.BL.Domain.Widgets;
 using BAR.UI.MVC.Models;
 using BAR.BL.Domain.Core;
-using Microsoft.Owin.BuilderProperties;
 
 namespace BAR.UI.MVC
 {
-  public class MvcApplication : System.Web.HttpApplication
-  {
-    protected void Application_Start()
-    {
-      AreaRegistration.RegisterAllAreas();
-      GlobalConfiguration.Configure(WebApiConfig.Register);
-      RouteConfig.RegisterRoutes(RouteTable.Routes);
-      BundleConfig.RegisterBundles(BundleTable.Bundles);
-      Mapper.Initialize(cfg => {
-        cfg.CreateMap<Item, ItemDTO>()
-          .ForMember(m => m.ItemId, opt => opt.MapFrom(src => src.ItemId))
-          .ForMember(m => m.Name, opt => opt.MapFrom(src => src.Name))
-          .ForMember(m => m.NumberOfMentions, opt => opt.MapFrom(src => src.NumberOfMentions))
-          .ForMember(m => m.SentimentNegative, opt => opt.MapFrom(src => src.SentimentNegative))
-          .ForMember(m => m.SentimentPositive, opt => opt.MapFrom(src => src.SentimentPositve))
-          .ForMember(m => m.TrendingPercentage, opt => opt.MapFrom(src => Math.Floor(src.TrendingPercentage)));
-        cfg.CreateMap<UserWidget, UserWidgetDTO>()
-          .ForMember(w => w.WidgetId, opt => opt.MapFrom(src => src.WidgetId))
-          .ForMember(w => w.Title, opt => opt.MapFrom(src => src.Title))
-          .ForMember(w => w.ColumnSpan, opt => opt.MapFrom(src => src.ColumnSpan))
-          .ForMember(w => w.RowSpan, opt => opt.MapFrom(src => src.RowSpan))
-          .ForMember(w => w.ColumnNumber, opt => opt.MapFrom(src => src.ColumnNumber))
-          .ForMember(w => w.RowNumber, opt => opt.MapFrom(src => src.RowNumber))
-          .ForMember(w => w.WidgetType, opt => opt.MapFrom(src => src.WidgetType))
-          .ForMember(w => w.DashboardId, opt => opt.MapFrom(src => src.Dashboard.DashboardId))
-          .ForMember(w => w.ItemIds, opt => opt.MapFrom(src => src.Items.Select(i => i.ItemId).ToList()));
-        cfg.CreateMap<Widget, UserWidgetDTO>()
-          .ForMember(w => w.WidgetId, opt => opt.MapFrom(src => src.WidgetId))
-          .ForMember(w => w.Title, opt => opt.MapFrom(src => src.Title))
-          .ForMember(w => w.ColumnSpan, opt => opt.MapFrom(src => src.ColumnSpan))
-          .ForMember(w => w.RowSpan, opt => opt.MapFrom(src => src.RowSpan))
-          .ForMember(w => w.ColumnNumber, opt => opt.MapFrom(src => src.ColumnNumber))
-          .ForMember(w => w.RowNumber, opt => opt.MapFrom(src => src.RowNumber))
-          .ForMember(w => w.WidgetType, opt => opt.MapFrom(src => src.WidgetType))
-          .ForMember(w => w.ItemIds, opt => opt.MapFrom(src => src.Items.Select(i => i.ItemId).ToList()));
-        cfg.CreateMap<Person, ItemViewModels.PersonViewModel>()
-          .ForMember(p => p.District, opt => opt.MapFrom(src => src.District))
-          .ForMember(p => p.Town, opt => opt.MapFrom(src => src.Area.Residence))
-          .ForMember(p => p.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth))
-          .ForMember(p => p.Gender, opt => opt.MapFrom(src => src.Gender))
-          .ForMember(p => p.Site, opt => opt.MapFrom(src => src.Site))
-          .ForMember(p => p.Level, opt => opt.MapFrom(src => src.Level))
-          .ForMember(p => p.OrganisationId, opt => opt.MapFrom(src => src.Organisation.ItemId))
-          .ForMember(p => p.OrganisationName, opt => opt.MapFrom(src => src.Organisation.Name))
-          .ForMember(p => p.Position, opt => opt.MapFrom(src => src.Position))
-          .ForMember(p => p.SocialMediaNames, opt => opt.MapFrom(src => src.SocialMediaNames));
-        cfg.CreateMap<Organisation, ItemViewModels.OrganisationViewModel>()
-          .ForMember(p => p.Site, opt => opt.MapFrom(src => src.Site))
-          .ForMember(p => p.PageTitle, opt => opt.MapFrom(src => src.Name))
-          .ForMember(p => p.SocialMediaNames, opt => opt.MapFrom(src => src.SocialMediaUrls));
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			Mapper.Initialize(cfg =>
+			{
+				//Mapping items
+				cfg.CreateMap<Item, ItemDTO>()
+				  .ForMember(m => m.ItemId, opt => opt.MapFrom(src => src.ItemId))
+				  .ForMember(m => m.Name, opt => opt.MapFrom(src => src.Name))
+				  .ForMember(m => m.NumberOfMentions, opt => opt.MapFrom(src => src.NumberOfMentions))
+				  .ForMember(m => m.SentimentNegative, opt => opt.MapFrom(src => src.SentimentNegative))
+				  .ForMember(m => m.SentimentPositive, opt => opt.MapFrom(src => src.SentimentPositve))
+				  .ForMember(m => m.TrendingPercentage, opt => opt.MapFrom(src => Math.Floor(src.TrendingPercentage)));
+
+				//Mapping UserWidgets
+				cfg.CreateMap<UserWidget, UserWidgetDTO>()
+				  .ForMember(w => w.WidgetId, opt => opt.MapFrom(src => src.WidgetId))
+				  .ForMember(w => w.Title, opt => opt.MapFrom(src => src.Title))
+				  .ForMember(w => w.ColumnSpan, opt => opt.MapFrom(src => src.ColumnSpan))
+				  .ForMember(w => w.RowSpan, opt => opt.MapFrom(src => src.RowSpan))
+				  .ForMember(w => w.ColumnNumber, opt => opt.MapFrom(src => src.ColumnNumber))
+				  .ForMember(w => w.RowNumber, opt => opt.MapFrom(src => src.RowNumber))
+				  .ForMember(w => w.WidgetType, opt => opt.MapFrom(src => src.WidgetType))
+				  .ForMember(w => w.DashboardId, opt => opt.MapFrom(src => src.Dashboard.DashboardId))
+				  .ForMember(w => w.ItemIds, opt => opt.MapFrom(src => src.Items.Select(i => i.ItemId).ToList()));
+
+				//Mapping widgets
+				cfg.CreateMap<Widget, UserWidgetDTO>()
+				  .ForMember(w => w.WidgetId, opt => opt.MapFrom(src => src.WidgetId))
+				  .ForMember(w => w.Title, opt => opt.MapFrom(src => src.Title))
+				  .ForMember(w => w.ColumnSpan, opt => opt.MapFrom(src => src.ColumnSpan))
+				  .ForMember(w => w.RowSpan, opt => opt.MapFrom(src => src.RowSpan))
+				  .ForMember(w => w.ColumnNumber, opt => opt.MapFrom(src => src.ColumnNumber))
+				  .ForMember(w => w.RowNumber, opt => opt.MapFrom(src => src.RowNumber))
+				  .ForMember(w => w.WidgetType, opt => opt.MapFrom(src => src.WidgetType))
+				  .ForMember(w => w.ItemIds, opt => opt.MapFrom(src => src.Items.Select(i => i.ItemId).ToList()));
+
+				//Mapping perons
+				cfg.CreateMap<Person, ItemViewModels.PersonViewModel>()
+				  .ForMember(p => p.District, opt => opt.MapFrom(src => src.District))
+				  .ForMember(p => p.Town, opt => opt.MapFrom(src => src.Area.Residence))
+				  .ForMember(p => p.DateOfBirth, opt => opt.MapFrom(src => src.DateOfBirth))
+				  .ForMember(p => p.Gender, opt => opt.MapFrom(src => src.Gender))
+				  .ForMember(p => p.Site, opt => opt.MapFrom(src => src.Site))
+				  .ForMember(p => p.Level, opt => opt.MapFrom(src => src.Level))
+				  .ForMember(p => p.OrganisationId, opt => opt.MapFrom(src => src.Organisation.ItemId))
+				  .ForMember(p => p.OrganisationName, opt => opt.MapFrom(src => src.Organisation.Name))
+				  .ForMember(p => p.Position, opt => opt.MapFrom(src => src.Position))
+				  .ForMember(p => p.SocialMediaNames, opt => opt.MapFrom(src => src.SocialMediaNames));
+
+				//Mapping organisations
+				cfg.CreateMap<Organisation, ItemViewModels.OrganisationViewModel>()
+				  .ForMember(p => p.Site, opt => opt.MapFrom(src => src.Site))
+				  .ForMember(p => p.PageTitle, opt => opt.MapFrom(src => src.Name))
+				  .ForMember(p => p.SocialMediaNames, opt => opt.MapFrom(src => src.SocialMediaUrls));
+
+				//Mapping themes
 				cfg.CreateMap<Theme, ItemViewModels.ThemeViewModel>()
 					.ForMember(p => p.Keywords, opt => opt.MapFrom(src => src.Keywords));
+
+				//Mapping widgetdata
 				cfg.CreateMap<WidgetData, WidgetDataDTO>()
-          .ForMember(p => p.WidgetDataId, opt => opt.MapFrom(src => src.WidgetDataId))
-          .ForMember(p => p.GraphValues, opt => opt.MapFrom(src => src.GraphValues))
-          .ForMember(p => p.KeyValue, opt => opt.MapFrom(src => src.KeyValue))
-          .ForMember(p => p.WidgetId, opt => opt.MapFrom(src => src.Widget.WidgetId));
-     
-          //Mapping customization
+					.ForMember(p => p.WidgetDataId, opt => opt.MapFrom(src => src.WidgetDataId))
+					.ForMember(p => p.GraphValues, opt => opt.MapFrom(src => src.GraphValues))
+					.ForMember(p => p.KeyValue, opt => opt.MapFrom(src => src.KeyValue))
+					.ForMember(p => p.WidgetId, opt => opt.MapFrom(src => src.Widget.WidgetId));
+
+				//Mapping customization
 				cfg.CreateMap<Customization, CustomizationViewModel>()
 					.ForMember(m => m.CustomizationId, opt => opt.MapFrom(src => src.CustomizationId))
 					//Mapping Page colors
@@ -100,17 +111,19 @@ namespace BAR.UI.MVC
 					.ForMember(m => m.City, opt => opt.MapFrom(src => src.City))
 					.ForMember(m => m.Country, opt => opt.MapFrom(src => src.Country))
 					.ForMember(m => m.Email, opt => opt.MapFrom(src => src.Email));
-        cfg.CreateMap<UserWidget, UserWidgetViewModel>()
-          .ForMember(w => w.WidgetId, opt => opt.MapFrom(src => src.WidgetId))
-          .ForMember(w => w.Title, opt => opt.MapFrom(src => src.Title))
-          .ForMember(w => w.ColumnSpan, opt => opt.MapFrom(src => src.ColumnSpan))
-          .ForMember(w => w.RowSpan, opt => opt.MapFrom(src => src.RowSpan))
-          .ForMember(w => w.ColumnNumber, opt => opt.MapFrom(src => src.ColumnNumber))
-          .ForMember(w => w.RowNumber, opt => opt.MapFrom(src => src.RowNumber))
-          .ForMember(w => w.WidgetType, opt => opt.MapFrom(src => src.WidgetType))
-          .ForMember(w => w.DashboardId, opt => opt.MapFrom(src => src.Dashboard.DashboardId))
-          .ForMember(w => w.ItemIds, opt => opt.MapFrom(src => src.Items.Select(i => i.ItemId).ToList()));
-      });
-    }
-  }
+				
+				//Mapping user widgets
+				cfg.CreateMap<UserWidget, UserWidgetViewModel>()
+				  .ForMember(w => w.WidgetId, opt => opt.MapFrom(src => src.WidgetId))
+				  .ForMember(w => w.Title, opt => opt.MapFrom(src => src.Title))
+				  .ForMember(w => w.ColumnSpan, opt => opt.MapFrom(src => src.ColumnSpan))
+				  .ForMember(w => w.RowSpan, opt => opt.MapFrom(src => src.RowSpan))
+				  .ForMember(w => w.ColumnNumber, opt => opt.MapFrom(src => src.ColumnNumber))
+				  .ForMember(w => w.RowNumber, opt => opt.MapFrom(src => src.RowNumber))
+				  .ForMember(w => w.WidgetType, opt => opt.MapFrom(src => src.WidgetType))
+				  .ForMember(w => w.DashboardId, opt => opt.MapFrom(src => src.Dashboard.DashboardId))
+				  .ForMember(w => w.ItemIds, opt => opt.MapFrom(src => src.Items.Select(i => i.ItemId).ToList()));
+			});
+		}
+	}
 }
