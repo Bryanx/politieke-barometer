071159b86b2bd0fa5fc8e91049cad37130f2afcf
Anthony <torfs.anthony@hotmail.com>
2018-05-21 09:27:41 +0200

    refactored colors on graphs and saving method. + weekly-review graphs can now be saved on dashboard

--- UI-MVC/Content/build/js/homepage.js
+++ UI-MVC/Content/build/js/homepage.js
@@ -19,6 +19,17 @@ var DARKCOLORS = [
     'rgb(177, 51, 151)'
 ];
 
+function getRandomColor(datasets) {
+    if (datasets.length >= COLORS.length) return COLORS.length-1;
+    setColors = [];
+    datasets.forEach(d => setColors.push(COLORS.indexOf(d.backgroundColor)));
+    let newColor = setColors[0];
+    while (setColors.includes(newColor)) {
+        newColor = Math.floor((Math.random() * COLORS.length));
+    }
+    return newColor;
+}
+
 /* ************* HOMEPAGE ************** */
 /* Checking how much you have scrolled. if it is past 60% of the screen or 500px then show the navbar otherwise hide it */
 function checkScroll() {
@@ -94,9 +105,7 @@ let TwitterFeed = function (trendings) {
 };
 
 /* ---------- Trending chart ----------*/
-var charts1 = []
-var charts2 = []
-var charts3 = [];
+var charts = [];
 let AddChart = function (name, widgetId, labels, values, itemType, chartType="line") {
     let el = "trending-" + itemType + "-graph";
     let c = new Chart(document.getElementById(el), {
@@ -118,13 +127,7 @@ let AddChart = function (name, widgetId, labels, values, itemType, chartType="li
             maintainAspectRatio: false,
         }
     });
-    if (itemType === 1) {
-        charts1.push(c);
-    } else if (itemType === 2) {
-        charts2.push(c);
-    } else {
-        charts3.push(c);
-    }
+    charts.push(c);
         
 };
 
@@ -135,17 +138,17 @@ let getGraph = function(name, itemId, widgetId, itemType) {
         url: "/api/GetGraphs/" + itemId + "/" + widgetId,
         dataType: "json",
         success: data => {
-            let d = $('#' + itemType).data("widgetId");
-            $('#' + itemType).data("widgetId", d + " " + itemId);
+            let d = $('#' + (itemType - 1)).data("widgetId");
+            $('#' + (itemType - 1)).data("widgetId", d + " " + itemId);
             switch (itemType){
                 case 1:
-                    charts1[0] == null ? AddChart(name, data[0].WidgetId, data[0].GraphValues.map(g => g.Value).reverse().slice(-14), data[0].GraphValues.map(g => g.NumberOfTimes).reverse().slice(-14), itemType) : AddDataSet(charts1[0], name, data[0].GraphValues.map(g => g.NumberOfTimes).reverse().slice(-14), itemType);
+                    charts.find(c => c.config.id === 0)  === undefined ? AddChart(name, 0, data[0].GraphValues.map(g => g.Value).reverse().slice(-14), data[0].GraphValues.map(g => g.NumberOfTimes).reverse().slice(-14), itemType) : AddDataSet(charts.find(c => c.config.id === 0), name, data[0].GraphValues.map(g => g.NumberOfTimes).reverse().slice(-14), itemType);
                     break;
                 case 2:
-                    charts2[0] == null ? AddChart(name, data[0].WidgetId, data[0].GraphValues.map(g => g.Value).reverse().slice(-14), data[0].GraphValues.map(g => g.NumberOfTimes).reverse().slice(-14), itemType) : AddDataSet(charts2[0], name, data[0].GraphValues.map(g => g.NumberOfTimes).reverse().slice(-14), itemType);
+                    charts.find(c => c.config.id === 1)  === undefined ? AddChart(name, 1, data[0].GraphValues.map(g => g.Value).reverse().slice(-14), data[0].GraphValues.map(g => g.NumberOfTimes).reverse().slice(-14), itemType) : AddDataSet( charts.find(c => c.config.id === 1), name, data[0].GraphValues.map(g => g.NumberOfTimes).reverse().slice(-14), itemType);
                     break;
                 case 3:
-                    charts3[0] == null ? AddChart(name, data[0].WidgetId, data[0].GraphValues.map(g => g.Value).reverse().slice(-14), data[0].GraphValues.map(g => g.NumberOfTimes).reverse().slice(-14), itemType) : AddDataSet(charts3[0], name, data[0].GraphValues.map(g => g.NumberOfTimes).reverse().slice(-14), itemType);
+                    charts.find(c => c.config.id === 2)  === undefined ? AddChart(name, 2, data[0].GraphValues.map(g => g.Value).reverse().slice(-14), data[0].GraphValues.map(g => g.NumberOfTimes).reverse().slice(-14), itemType) : AddDataSet( charts.find(c => c.config.id === 2), name, data[0].GraphValues.map(g => g.NumberOfTimes).reverse().slice(-14), itemType);
                     break;
             }
         },
@@ -155,7 +158,7 @@ let getGraph = function(name, itemId, widgetId, itemType) {
 /*--------- Adding data for trending chart ----------*/    
 
 let AddDataSet = function (chart, name, values) {
-    let random = Math.floor(Math.random()*(COLORS.length -1)); // Gets random color
+    let random = getRandomColor(chart.config.data.datasets); // Gets random color
     let newDataset = {
         label: name,
         borderColor: COLORS[random],
@@ -195,8 +198,10 @@ let WeeklyReview = function (name, itemId, widgetId, itemType) {
         success: data => {
             let random = Math.floor(Math.random()*(COLORS.length -1)); // Gets random color
             let el = "weeklyReview-" + itemType;
-            new Chart(document.getElementById(el), {
-                id: data[0].WidgetId,
+            let d = $('#' + (itemType+2)).data("widgetId");
+            $('#' + (itemType+2)).data("widgetId", d + " " + itemId);
+            let c = new Chart(document.getElementById(el), {
+                id: (itemType+2),
                 type: "line",
                 data: {
                     labels: data[0].GraphValues.map(g => g.Value).reverse().slice(-14),
@@ -214,23 +219,20 @@ let WeeklyReview = function (name, itemId, widgetId, itemType) {
                     maintainAspectRatio: false,
                 }
             });
+            charts.push(c)
         },
         fail: d => console.log(d)
     });
 };
 
-/*------ Getting chart -------*/
-let getChart = function (id) {
-    switch (id) {
-        case "1": return charts1;
-        case "2": return charts2;
-        case "3": return charts3;
-    }
+/*---------- finding chart ---------- */
+let findingChart = function (id) {
+  return charts.find(chart => chart.config.id == id);  
 };
 
 /*----- Saving widget to dashboard -----*/
 let makingJSON = function (e){
-    let c = getChart(e.target.id);
+    let c = findingChart(e.target.id);
     let list = $('#' + e.target.id).data("widgetId").toString().split(" ");
     let ids = [];
         $.each(list, function( index, value ) {
@@ -238,7 +240,7 @@ let makingJSON = function (e){
     });
        let json = {
         ItemIds: ids, 
-        GraphType: c[0].config.type,
+        GraphType: c.config.type,
         PropertyTag: "Number of mentions"
     };
     addWidgetToDashboard(json);
