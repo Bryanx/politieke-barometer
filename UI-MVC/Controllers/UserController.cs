f2704d45d412d1e8a45f865006de86525932a481
Rémi <remi.1998@hotmail.com>
2018-05-25 13:34:24 +0200

    Removed unused security feature (concerning xsrf)

--- UI-MVC/Controllers/UserController.cs
+++ UI-MVC/Controllers/UserController.cs
@@ -648,9 +648,6 @@ namespace BAR.UI.MVC.Controllers
 
 		#region Helpers
 
-		// Used for XSRF protection when adding external logins
-		private const string XsrfKey = "XsrfId";
-
 		private ActionResult RedirectToLocal(string returnUrl)
 		{
 			if (Url.IsLocalUrl(returnUrl))
@@ -686,18 +683,6 @@ namespace BAR.UI.MVC.Controllers
 			public string LoginProvider { get; set; }
 			public string RedirectUri { get; set; }
 			public string UserId { get; set; }
-
-			public override void ExecuteResult(ControllerContext context)
-			{
-				var properties = new AuthenticationProperties { RedirectUri = RedirectUri };
-				if (UserId != null)
-				{
-					properties.Dictionary[XsrfKey] = UserId;
-				}
-
-				context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
-			}
-		}
 
 		#endregion
 	}
