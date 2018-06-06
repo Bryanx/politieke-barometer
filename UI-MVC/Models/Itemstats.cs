7f9afd9b85d4e0810eaba3b561a8405db7acd45d
Maarten Jorens <maarten.jorens@skynet.be>
2018-06-06 14:23:42 +0200

    Notenbox (#169)
    * JSON notenbox
    
    * Notenbox macheert <3

---
+++ UI-MVC/Models/Itemstats.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BAR.UI.MVC.Controllers
+{
+	public class Itemstats
+	{
+		public int Male { get; set; }
+		public int Female { get; set; }
+		public int GenderUnknown { get; set; }
+
+		public int Old { get; set; }
+		public int Young { get; set; }
+		public int AgeUnknown { get; set; }
+	}
+}
